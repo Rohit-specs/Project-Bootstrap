cdfe34498bdb549248a386e3d8b55304bbfca9ff
rohit-specs <rohit5592006@gmail.com>
2026-03-05 15:19:47 +0530

    vite updated

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,16 @@ import { resolve } from 'path'
 export default {
   root: resolve(__dirname, 'src'),
   build: {
-    outDir: '../dist'
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'src/index.html'),
+        contact: path.resolve(__dirname, 'src/contact.html'),
+        courses: path.resolve(__dirname, 'src/courses.html'),
+        coursesdetail: path.resolve(__dirname, 'src/courses-details.html'),
+        login: path.resolve(__dirname, 'src/login.html'),
+        signup: path.resolve(__dirname, 'src/signup.html'),
+      },
+    },
   },
   server: {
     port: 8080
