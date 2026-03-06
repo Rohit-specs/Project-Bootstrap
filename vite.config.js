986e1a72c005e2783fc584f0a32bbfd8bf6352cc
rohit-specs <rohit5592006@gmail.com>
2026-03-06 14:25:29 +0530

    updated

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,83 @@
-import { defineConfig } from 'vite';
-import path from 'path';
+// import { defineConfig } from 'vite';
+// import path from 'path';
 
-export default defineConfig({
+// export default defineConfig({
+//   build: {
+//     rollupOptions: {
+//       input: {
+//   main: path.resolve(__dirname, 'src/index.html'),
+//   contact: path.resolve(__dirname, 'src/contact.html'),
+//   courses: path.resolve(__dirname, 'src/courses.html'),
+//   coursesdetail: path.resolve(__dirname, 'src/courses-detail.html'),
+//   login: path.resolve(__dirname, 'src/login.html'),
+//   signup: path.resolve(__dirname, 'src/signup.html'),
+// },
+//     },
+//   },
+//   server: { 
+//     port: 8080, // dev server port
+//   },
+//   css: {
+//     preprocessorOptions: {
+//       scss: {
+//         // Silence deprecation warnings (true to silence all)
+//         silenceDeprecationWarnings: true,
+//       },
+//     },
+//   },
+// });
+
+
+import { resolve } from 'path'
+
+export default {
+  root: resolve(__dirname, 'src'),
   build: {
-    rollupOptions: {
-      input: {
-  main: path.resolve(__dirname, 'src/index.html'),
-  contact: path.resolve(__dirname, 'src/contact.html'),
-  courses: path.resolve(__dirname, 'src/courses.html'),
-  coursesdetail: path.resolve(__dirname, 'src/courses-detail.html'),
-  login: path.resolve(__dirname, 'src/login.html'),
-  signup: path.resolve(__dirname, 'src/signup.html'),
-},
-    },
+    outDir: '../dist'
   },
-  server: { 
-    port: 8080, // dev server port
+  server: {
+    port: 8080
   },
+  // Optional: Silence Sass deprecation warnings. See note below.
   css: {
-    preprocessorOptions: {
-      scss: {
-        // Silence deprecation warnings (true to silence all)
-        silenceDeprecationWarnings: true,
-      },
-    },
+     preprocessorOptions: {
+        scss: {
+          silenceDeprecations: [
+            'import',
+            'mixed-decls',
+            'color-functions',
+            'global-builtin',
+          ],
+        },
+     },
   },
-});
+}
+
+// import { defineConfig } from 'vite';
+// import path from 'path';
+
+// export default defineConfig({
+//   build: {
+//     rollupOptions: {
+//       input: {
+//         main: path.resolve(__dirname, 'src/index.html'),
+//         contact: path.resolve(__dirname, 'src/contact.html'),
+//         courses: path.resolve(__dirname, 'src/courses.html'),
+//         coursesdetail: path.resolve(__dirname, 'src/courses-details.html'),
+//         login: path.resolve(__dirname, 'src/login.html'),
+//         signup: path.resolve(__dirname, 'src/signup.html'),
+//       },
+//     },
+//   },
+//   server: {
+//     port: 8080, // dev server port
+//   },
+//   css: {
+//     preprocessorOptions: {
+//       scss: {
+//         // Silence deprecation warnings (true to silence all)
+//         silenceDeprecationWarnings: true,
+//       },
+//     },
+//   },
+// });
