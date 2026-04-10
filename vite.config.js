78399512c16fbe9e8cf37d69b3ff088cd5d246ef
Rohit kumar <rohit5592006@gmail.com>
2026-04-10 21:38:57 +0530

    updated

--- vite.config.js
+++ vite.config.js
@@ -58,7 +58,9 @@ import { defineConfig } from 'vite'
 import { resolve } from 'path'
 
 export default defineConfig({
+  root: 'src',
   build: {
+    outDir: '../dist',
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
