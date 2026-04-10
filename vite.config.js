11f90d7b2aeb7d89c7a0fdc0e24cc3ed45f090b2
Rohit kumar <rohit5592006@gmail.com>
2026-04-10 21:44:24 +0530

    updated

--- vite.config.js
+++ vite.config.js
@@ -54,20 +54,21 @@
 //   },
 // }
 
-import { defineConfig } from 'vite'
+// import { defineConfig } from 'vite'
 import { resolve } from 'path'
 
 export default defineConfig({
   root: 'src',
   build: {
     outDir: '../dist',
+    emptyOutDir: true,
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'),
-        courses: resolve(__dirname, 'courses.html'),
-        contact: resolve(__dirname, 'contact.html'),
-        login: resolve(__dirname, 'login.html'),
-        signup: resolve(__dirname, 'signup.html'),
+        main: resolve(__dirname, 'src/index.html'),
+        courses: resolve(__dirname, 'src/courses.html'),
+        contact: resolve(__dirname, 'src/contact.html'),
+        login: resolve(__dirname, 'src/login.html'),
+        signup: resolve(__dirname, 'src/signup.html')
       }
     }
   }
